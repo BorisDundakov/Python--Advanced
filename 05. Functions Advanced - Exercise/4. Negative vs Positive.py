5f0379f6f9a03dadb0ba37bf6690862054d86233
Boris Dundakov <71731579+b0reto@users.noreply.github.com>
2021-06-07 20:36:31 +0300

    Update 4. Negative vs Positive.py

--- 05. Functions Advanced - Exercise/4. Negative vs Positive.py
+++ 05. Functions Advanced - Exercise/4. Negative vs Positive.py
@@ -2,19 +2,15 @@ nums_list = [int(el) for el in input().split()]
 
 
 def neg_vs_pos(numbers_list):
-    negatives_list = []
-    posiives_list = []
-
+    negatives = []
+    positives = []
     for el in range(len(numbers_list)):
-        if numbers_list[el] > 0:
-            posiives_list.append(numbers_list[el])
-        else:
-            negatives_list.append(numbers_list[el])
+        negatives = list(filter(lambda number: number < 0, numbers_list))
+        positives = list(filter(lambda number: number > 0, numbers_list))
+    print(sum(negatives))
+    print(sum(positives))
 
-    print(sum(negatives_list))
-    print(sum(posiives_list))
-
-    if abs(sum(negatives_list)) > sum(posiives_list):
+    if abs(sum(negatives)) > sum(positives):
         print('The negatives are stronger than the positives')
     else:
         print('The positives are stronger than the negatives')
